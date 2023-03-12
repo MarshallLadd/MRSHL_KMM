e3cf9bffd0f833e50bff497f4a8238d5d607c873
Marshall Ladd <marshall.ladd@pm.me>
2023-03-12 00:08:03 -0600

    colors

---
+++ androidApp/src/main/java/me/pm/marshall/ladd/mrshl/android/presentation/theme/Color.kt
@@ -0,0 +1,54 @@
+package me.pm.marshall.ladd.mrshl.android.presentation.theme
+
+import androidx.compose.material.Colors
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import me.pm.marshall.ladd.mrshl.presentation.core.MrshlColors
+
+val lightColorPalette = lightColors(
+    primary = Color(MrshlColors.White),
+    primaryVariant = Color(MrshlColors.LightKeyboardGrey),
+    secondary = Color(MrshlColors.Green),
+    secondaryVariant = Color(MrshlColors.Yellow),
+    background = Color(MrshlColors.White),
+    surface = Color(MrshlColors.White),
+    onPrimary = Color(MrshlColors.Black),
+    onSecondary = Color(MrshlColors.White),
+    onSurface = Color(MrshlColors.Black),
+    onBackground = Color(MrshlColors.Black),
+    error = Color(MrshlColors.Error),
+    onError = Color(MrshlColors.White),
+)
+
+val darkColorPalette = darkColors(
+    primary = Color(MrshlColors.DarkBackground),
+    primaryVariant = Color(MrshlColors.DarkKeyboardGrey),
+    secondary = Color(MrshlColors.Green),
+    secondaryVariant = Color(MrshlColors.Yellow),
+    background = Color(MrshlColors.DarkBackground),
+    surface = Color(MrshlColors.DarkBackground),
+    onPrimary = Color(MrshlColors.White),
+    onSecondary = Color(MrshlColors.White),
+    onSurface = Color(MrshlColors.White),
+    onBackground = Color(MrshlColors.White),
+    error = Color(MrshlColors.Error),
+    onError = Color(MrshlColors.White),
+)
+
+@get:Composable
+val Colors.goodLetterGoodPlaceBackground: Color
+    get() = Color(MrshlColors.Green)
+
+@get:Composable
+val Colors.goodLetterBadPlaceBackground: Color
+    get() = Color(MrshlColors.Yellow)
+
+@get:Composable
+val Colors.badLetterBackground: Color
+    get() = if (isLight) Color(MrshlColors.LightBadGuessBackground) else Color(MrshlColors.DarkBadGuessBackground)
+
+@get:Composable
+val Colors.onLetterGuess: Color
+    get() = Color(MrshlColors.White)
---
+++ shared/src/commonMain/kotlin/me/pm/marshall/ladd/mrshl/presentation/core/MrshlColors.kt
@@ -0,0 +1,16 @@
+package me.pm.marshall.ladd.mrshl.presentation.core
+
+object MrshlColors {
+    const val White = 0xFFFFFFFF
+    const val Black = 0xFF000000
+    const val Green = 0xFF6AAA64
+    const val Yellow = 0xFFC9B458
+    const val LightKeyboardGrey = 0xFFD3D6DA
+    const val DarkKeyboardGrey = 0xFF818384
+    const val DarkBackground = 0xFF121213
+    const val LightBorderEmpty = 0xFFD3D6DA
+    const val DarkBorderEmpty = 0xFF3A3A3C
+    const val LightBadGuessBackground = 0xFF787C7E
+    const val DarkBadGuessBackground = 0xFF3A3A3C
+    const val Error = 0xFFEA0548
+}
